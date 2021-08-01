7f589affaa61ee8c2361d20acf9c157b2a28bfb3
nargok <nargok@gmail.com>
2021-08-01 11:51:25 +0900

    tailwindcss setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './tailwind.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-4xl text-green-700 text-center font-semibold">Hello, Tailwind CSS</h1>
+      <div className="grid justify-items-center py-8">
+        <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">
+          ボタン
+        </button>
+      </div>
     </div>
   );
 }
